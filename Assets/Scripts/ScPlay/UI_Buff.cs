28cc84683897d9a014045e26da490a67ae8076fd
kula1027 <jeff5289@naver.com>
2015-07-19 15:09:06 +0900

    v3 UI update UI관련 사항 수정한 (아마도)최신 버전입니다 우상단 메뉴 구현, 하단 슬롯바 정상 작동하도록 수정, 캐릭터 입력을 위한 UI틀 구현, UI간의 비율 조정했고 좌상단 status도 버프 숫자 늘이고 줄일수 있고 게이지들 컨트롤하는 함수들 작성했습니다

---
+++ Assets/Scripts/ScPlay/UI_Buff.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Buff : MonoBehaviour {
+	private GameObject[] buff = new GameObject[12];
+	private int buffCount;
+
+	void Start () {
+		for (int loop = 0; loop < 12; loop++) {
+			buff[loop] = GameObject.Find("Buff (" + loop + ")");
+			buff[loop].SetActive(false);
+		}
+
+		buffCount = 0;
+	}
+
+	void AddBuff(){
+		buff [buffCount].SetActive (true);
+		buffCount++;
+	}
+
+	void DeleteBuff(int index){
+		if (index > buffCount) {
+			Debug.Log ("index exceed buffCount");
+			return;
+		} else {
+
+		}
+	}
+}
